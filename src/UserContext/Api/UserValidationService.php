d3387ac322809f0e9883cdfb0fc4015c0f99c375
John Porter <john@designermonkey.co.uk>
2017-01-25 22:51:10 +0000

    Add service to validate user data

---
+++ src/UserContext/Api/UserValidationService.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Example\UserContext\Api;
+
+use Example\Model\Specification;
+use Example\UserContext\Model\UserRepository;
+use Example\UserContext\Model\UserFactory;
+
+class UserValidationService
+{
+    /**
+     * @var UserFactory
+     */
+    private $userFactory;
+
+    /**
+     * @var Specification
+     */
+    private $passwordSpecification;
+
+    /**
+     * @param UserFactory    $userFactory
+     */
+    public function __construct(UserFactory $userFactory, Specification $passwordSpecification)
+    {
+        $this->userFactory = $userFactory;
+        $this->passwordSpecification = $passwordSpecification;
+    }
+
+    /**
+     * @param  string $emailAddress
+     * @return array
+     */
+    public function validateEmailAddress(string $emailAddress): array
+    {
+        try {
+            $createdEmailAddress = $this->userFactory->createEmailAddress($emailAddress);
+            $this->passwordSpecification->isSatisfiedBy($createdPassword);
+        } catch (InvalidArgumentException $exception) {
+            return [
+                'message' => $exception->getMessage(),
+                'valid' => false,
+                'emailAddress' => $emailAddress
+            ];
+        }
+
+        return [
+            'valid' => true,
+            'emailAddress' => (string) $createdEmailAddress
+        ];
+    }
+
+    /**
+     * @param  string $password
+     * @return array
+     */
+    public function validatePassword(string $password): array
+    {
+        try {
+            $createdPassword = $this->userFactory->createPassword($password);
+        } catch (InvalidArgumentException $exception) {
+            return [
+                'message' => $exception->getMessage(),
+                'valid' => false,
+                'password' => $password
+            ];
+        }
+
+        return [
+            'valid' => true,
+            'password' => (string) $createdPassword
+        ];
+    }
+
+    /**
+     * @param  string $name
+     * @return array
+     */
+    public function validateName(string $name): array
+    {
+        try {
+            $createdName = $this->userFactory->createName($name);
+        } catch (InvalidArgumentException $exception) {
+            return [
+                'message' => $exception->getMessage(),
+                'valid' => false,
+                'name' => $name
+            ];
+        }
+
+        return [
+            'valid' => true,
+            'name' => (string) $createdName
+        ];
+    }
+
+    public function validateDateOfBirth(string $dateOfBirth): array
+    {
+        try {
+            $parts = explode('/', $dateOfBirth);
+            $createdDateOfBirth = $this->userFactory->createDateOfBirth(...$parts);
+        } catch (InvalidArgumentException $exception) {
+            return [
+                'message' => $exception->getMessage(),
+                'valid' => false,
+                'dateOfBirth' => $dateOfBirth
+            ];
+        }
+
+        return [
+            'valid' => true,
+            'dateOfBirth' => (string) $createDateOfBirth
+        ];
+    }
+}
